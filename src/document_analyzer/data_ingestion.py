35fb2d549b3f1267b770e64bd43791246ce931cf
nandhakumarmurugesan <nandha.kumar.murugesan.ai@gmail.com>
2025-09-08 17:42:27 +0530

    updated document ingestion of doc_analyzer

--- src/document_analyzer/data_ingestion.py
+++ src/document_analyzer/data_ingestion.py
@@ -10,12 +10,44 @@ class DocumentHandler:
     Handles PDF saving and reading operations
     Automatically logs all actions and supports session-based organizations
     """
-    def __init__():
-        pass
+    def __init__(self,data_dir=None,session_id=None):
+        #session history for every instance
+        try:
+            self.CustomLogger().get_logger(__name__)
+            #check data directory from env variable or use default path
+            self.data_dir = data_dir or os.getenv(
+                                    "DATA_STORAGE_PATH",
+                                    os.path.join(os.getcwd(),"data","document_analysis") 
+            )
+            self.session_id = session_id or f"session_{datetime.utcnow().strftime('%Y%m%d_%H%M%S')}_{uuid.uuid4().hex[:8]}" 
+            
+            #create base session directory
+            self.session_path = os.path.join(self.data_dir,self.session_id)
+            os.makedirs(self.session_path,exist_ok=True) 
+            
+            self.log.info("PDFHandler initialized", session_id=self.session_id, session_path=self.session_path)
+        
+        except Exception as e:        
+            self.log.error(f"Error during PDFHandler initialization: {e}")
+            raise DocumentPortalException("Error initializing DocumentHadler", e) from e
     
     def save_pdf():
-        pass
+        try:
+            pass
+        except Exception as e:        
+            self.log.error(f"Error during saving PDF: {e}")
+            raise DocumentPortalException("Error saving PDF", e) from e
     
     def read_pdf():
-        pass
-    
+        try:
+            pass
+        except Exception as e:
+            self.log.error(f"Error during reading PDF: {e}")
+            raise DocumentPortalException("Error reading PDF", e) from e
+        
+if __name__ == "__main__":
+    handler = DocumentHandler()
+    print(f"Session ID: {handler.session_id}")
+    print(f"Session Path: {handler.session_path}")
+    #handler.save_pdf()
+    #handler.read_pdf()
